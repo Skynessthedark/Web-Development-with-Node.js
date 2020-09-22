591a51a928b95bc1ada2cb435ad17c820b0bd7c6
Skynessthedark <gokseninmert.9@gmail.com>
2020-09-23 02:17:07 +0300

    New Changes

--- cheat-sheet.js
+++ cheat-sheet.js
@@ -55,3 +55,94 @@ var insan = {
 insan.boy = 163
 console.log(insan)
 console.log(insan.fnk())
+
+//var - let farkı: var, scope karmaşasına yol açabilir; bu yüzden let kullanılır. Kısacası kod karışıklığını engeller.
+
+//template literal -> `${degisken}`
+let name  = "Göksenin"
+console.log(`Benim adim ${name}`)
+
+//Destructing -> tek tek key'leri almak yerine istediklerimizi aynı anda çekmek için destructing yöntemi kullanırız.
+// Key isimleri ile çektiğimiz isimler aynı olmalı.
+let degerler = {
+    d1 : "Deger1",
+    d2 : "Deger2",
+    d3 : "Deger3"
+}
+
+let {d1, d2} = degerler
+console.log(d1, d2)
+
+//spread operator -> dizinin tüm elemanlarını yazdırmada kullanılır. (...array)
+
+let array = [1, 2, 3]
+console.log(...array)
+
+//rest operator -> geri kalan indislerdeki değerleri yazdırır. (...rest)
+
+let [deger1, ...rest] = array
+console.log(rest)
+console.log(...rest)
+
+//Promise yapısı -> callback hell karmaşasından kurtulmaya yarayan yöntem.
+//Parametre olarak, resolve ve reject parametreli bir fonksiyon alır. Daha sonra çağırıldığında .then(func(resolve_data)) ve .catch(func(reject_data)) kullanılarak veri karmaşası önlenmiş olur.
+//İlk kullanımdan sonraki .then() in aldığı parametre bir öncekinin return ettiği veridir.
+
+let asenkfonk = (sayi) => {
+    return new Promise((resolve, reject) => {
+        if(sayi === 5)
+            resolve("Sayi kabul edildi.")
+        else
+            reject("Sayi reddedildi.")
+    })
+}
+
+asenkfonk(5)
+    .then((data) =>{
+        console.log(data)
+        return 1
+    })
+    .then((data) => {
+        console.log(data)
+        return 2
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
+    //async - await yapısı -> callback hell ve promise chain yöntemleriyle aynı işlevdedir. Fonksiyonun içindeki her satır sırayla ilerler.
+    //async - await kullanırken hata yakalamak(reject) için try catch kullanılır.
+    
+    let user = {id: 1, name: "ayşe"}
+    let friends = [{id: 2, name: "fatma"}, {id: 3, name: "hayriye"}]
+
+    const getUser = () =>{
+        return new Promise((resolve, reject) =>{
+            setTimeout((
+                resolve(user)
+            ),5000)
+        })
+    }
+
+    const getFriends = (userId) =>{
+        return new Promise((resolve, reject) =>{
+            setTimeout((
+                resolve(friends)
+            ), 6000)
+        })
+    }
+
+    const asenkronakis = async function() {
+        console.log("Get user")
+        let user = await getUser()
+        console.log(user)
+        console.log("User done.")
+
+        console.log("Get friends")
+        let friends = await getFriends(user.id)
+        console.log(friends)
+        console.log("Friends done.")
+
+    }
+
+asenkronakis()
